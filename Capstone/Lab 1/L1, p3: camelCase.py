fb853d8a965f9afbffdb504596d9d70c688d384e
zacharia <zachariasalad>
2023-01-23 01:01:28 -0600

    testing

--- Capstone/Lab 1/L1, p3: camelCase.py
+++ Capstone/Lab 1/L1, p3: camelCase.py
@@ -1,1 +1,6 @@
-sentence = ''ad
+sentence = input('Input words to camelCase: ')
+camelCased = ''
+splitSentence = sentence.split()
+for word in splitSentence:
+    camelCased += word.capitalize()
+print(camelCased[0].lower() + camelCased[1:])

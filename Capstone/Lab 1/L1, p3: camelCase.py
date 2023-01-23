caceeb9e74f72aeeea973f0d473bc3795d238316
zacharia <zachariasalad>
2023-01-23 01:03:58 -0600

    Lab 1

--- Capstone/Lab 1/L1, p3: camelCase.py
+++ Capstone/Lab 1/L1, p3: camelCase.py
@@ -3,4 +3,4 @@ camelCased = ''
 splitSentence = sentence.split()
 for word in splitSentence:
     camelCased += word.capitalize()
-print(camelCased[0].lower() + camelCased[1:])
+print('> ' + camelCased[0].lower() + camelCased[1:])
